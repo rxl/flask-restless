f3d23267b184c151064e80a521c7ba832e18402b
Matt Chisholm <matt@theory.org>
2013-04-23 16:27:32 +0200

    Improve has/any search. Fixes jfinkels/flask-restless#180

--- tests/test_views.py
+++ tests/test_views.py
@@ -1489,6 +1489,15 @@ class AssociationProxyTest(DatabaseTestBase):
         data = loads(response.data)
         self.assertEqual(data['num_results'], 0)
 
+        filters = {'filters':
+                       [{'name': 'chosen_images', 'op': 'any', 'val': {'name':'id', 'op':'eq', 'val':1}}]}
+        response = self.app.get('/api/product?q=' + dumps(filters))
+        self.assertEqual(response.status_code, 200)
+        data = loads(response.data)
+        self.assertEqual(data['num_results'], 0)
+
+
+
     def test_scalar(self):
         """Tests that association proxies to remote scalar attributes work
         correctly.
